1b6879c8271f08b9107dd673c9edd55e815987e9
Sandra Moen <sandramoen01@gmail.com>
2021-07-14 11:44:57 +0200

    minor project fixes

--- desktop/src/no/sandramoen/blipblop/desktop/DesktopLauncher.java
+++ desktop/src/no/sandramoen/blipblop/desktop/DesktopLauncher.java
@@ -4,22 +4,17 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import no.sandramoen.blipblop.BlipBlopGame;
 
-import java.awt.*;
-
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Blip Blop";
-		config.resizable = false;
-
-		// resolution
-		// Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		// double scale = dimension.getWidth() / dimension.getHeight();
+		float scale = 2.5f;
 		int LGg8ThinQHeight = 3120;
 		int LGg8ThinQWidth = 1440;
-		config.width = (int) (LGg8ThinQWidth / 2.5);
-		config.height = (int) (LGg8ThinQHeight / 2.5);
 
+		config.title = "Blip Blop!";
+		config.width = (int) (LGg8ThinQWidth / scale);
+		config.height = (int) (LGg8ThinQHeight / scale);
+		config.resizable = false;
 		new LwjglApplication(new BlipBlopGame(), config);
 	}
 }
