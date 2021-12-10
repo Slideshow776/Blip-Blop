6bbafe5fe0e5cdff626fce21268f577c85f471a0
Sandra Moen <sandramoen01@gmail.com>
2021-12-10 16:11:29 +0100

    resolution is now dynamic

--- desktop/src/no/sandramoen/blipblop/desktop/DesktopLauncher.java
+++ desktop/src/no/sandramoen/blipblop/desktop/DesktopLauncher.java
@@ -3,20 +3,27 @@ package no.sandramoen.blipblop.desktop;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import java.awt.Dimension;
+
 import no.sandramoen.blipblop.BlipBlopGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		float scale = 2.5f;
+
+		// resolution
 		int LGg8ThinQHeight = 3120;
 		int LGg8ThinQWidth = 1440;
+		Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		config.height = (int) (dimension.height / 1.1); // 10% less than height of screen
+		config.width = (int) (config.height / (LGg8ThinQHeight / LGg8ThinQWidth)); // width to mimic a phone
 
+		// miscellaneous
 		config.title = "Blip Blop!";
-		config.width = (int) (LGg8ThinQWidth / scale);
-		config.height = (int) (LGg8ThinQHeight / scale);
 		config.resizable = false;
 		config.addIcon("images/excluded/ic_launcher-desktop.png", Files.FileType.Internal);
+
 		new LwjglApplication(new BlipBlopGame(null, "en"), config);
 	}
 }
